a5c12d2996650fe752c9385ceba05e4e5c6b1922
annabaringer <akobanna@gmail.com>
2021-11-17 20:56:19 -0500

    random vid working

--- inspire_site/models.py
+++ inspire_site/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from embed_video.fields import EmbedVideoField
 
 class Item(models.Model):
-    video = EmbedVideoField()  # same like models.URLField()
+    video = EmbedVideoField()  

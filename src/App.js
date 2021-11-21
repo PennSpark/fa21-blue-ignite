7cd2fe8fde18408119ce9b54bdba42ec68dbffc6
jciardullo <64868372+jciardullo@users.noreply.github.com>
2021-11-21 15:27:18 -0500

    added calls to unsplash api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import SearchPhotos from "./photoSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">Photo API</h1>
+        <SearchPhotos />
+      </div>
+    </div>
+  );
+}
+
+export default App;

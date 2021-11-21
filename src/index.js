c4cc5d77ef3f48ae7e779bb2b82a53029a407fcc
BitLorax <willjhliang@gmail.com>
2021-11-20 20:55:40 -0500

    Set up http request for quotes

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+var destination = document.querySelector("#quote-container");
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url,false );
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+function parseResponse(response) {
+    return JSON.parse(response);
+}
+var response = parseResponse(httpGet("https://api.quotable.io/random"));
+var quote = response.content;
+var author = response.author;
+
+ReactDOM.render(
+    <div>
+        <p>{quote}</p>
+        <p>- {author}</p>
+    </div>,
+    destination
+);

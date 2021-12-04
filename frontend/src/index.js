e4460593f744aaec196bd988b2585966ad305b71
annabaringer <akobanna@gmail.com>
2021-12-04 17:10:01 -0500

    add video to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Canvas from "./Canvas";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import reportWebVitals from './reportWebVitals';
+import YouTube from 'react-youtube';
 
 var list_select = document.querySelector("#list-container");
 var quote_select = document.querySelector("#quote-container");
@@ -14,17 +15,52 @@ function httpGet(url) {
     xmlHttp.send(null);
     return xmlHttp.responseText;
 }
+
 function parseResponse(response) {
     return JSON.parse(response);
 }
+
+/* code for random quotes */
 var response = parseResponse(httpGet("https://api.quotable.io/random"));
 var quote = response.content;
 var author = response.author;
+
+/* code for random videos */
+function getRandomVid() {
+  var count = 10;
+  var API_KEY = 'AIzaSyDWTu1jFVq0sA8mqCTzrSKVb6FszG8ixrI';
+
+  // make a random search key, characters or digits
+  var random_choice = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+
+  for ( var i = 0; i < 3; i++ ) {
+    random_choice += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  // create a search query for the api
+  var api_data = 'https://www.googleapis.com/youtube/v3/search?key=' + API_KEY + '&maxResults=' + count + '&part=snippet&type=video&q=' + random_choice
+
+  // fetch that response
+  var video_response = parseResponse(httpGet(api_data));
+
+  // create a list of all of the video ids that were pulled
+  var id_list = [];
+  var items = video_response['items']
+
+  for ( var i = 0; i < count; i++ ) {
+    id_list.push(items[i]['id']['videoId']);
+  }
+
+  return id_list[Math.floor(Math.random() * id_list.length)];
+}
 
 ReactDOM.render(
     <div>
         <p>{quote}</p>
         <p>- {author}</p>
+        <YouTube videoId={getRandomVid()}/>
         <Canvas />
     </div>,
     quote_select
